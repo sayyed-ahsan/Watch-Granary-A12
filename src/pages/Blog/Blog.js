051392a11d35943702f250fd5092b00a7841225e
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-11-24 08:43:02 +0600

    blog

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            Blog page
+            <div className="flex flex-col w-full">
+                <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
+                <div className="divider"></div>
+                <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
+            </div>
         </div>
     );
 };
